9fcf8127f330265f62cb9bd5d332036daaa010bd
blvckcrow <conjerryst@gmail.com>
2024-10-06 14:41:15 +0700

    add file

---
+++ Bai1/test.php
@@ -0,0 +1,10 @@
+<?php
+    echo "<h1>Hello</h1>";
+    echo "<b>";
+    echo "<ul>";
+    echo "<li>Pho</li>";
+    echo "<li>Khanh cho</li>";
+    echo "<li>Bun</li>";
+    echo "</ul>";
+    echo 'hello';
+?>
